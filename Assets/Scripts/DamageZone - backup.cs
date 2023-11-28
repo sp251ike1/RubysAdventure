0af0fef2c14490b77e5fec1120bd2ad6e4445db5
DCJW <da196134@ucf.edu>
2023-11-28 18:24:55 -0500

    damage and health update

---
+++ Assets/Scripts/DamageZone - backup.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class DamageZone : MonoBehaviour 
+{
+    void OnTriggerStay2D(Collider2D other)
+    {
+        RubyController controller = other.GetComponent<RubyController>();
+
+        if (controller != null)
+        {
+            controller.ChangeHealth(-1);
+        }
+    }
+}
