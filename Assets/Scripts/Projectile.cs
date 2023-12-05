258dd9ba16d5fde31bdc66d9517070f660e41c71
DCJW <da196134@ucf.edu>
2023-12-05 18:28:05 -0500

    Daniel - Projectile Boomerang Script 12/5/23

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     Rigidbody2D rigidbody2d;
+    bool returning = false; // Flag to indicate if the projectile should return
 
     void Awake()
     {
@@ -18,24 +19,30 @@ public class Projectile : MonoBehaviour
 
     void Update()
     {
-        if (transform.position.magnitude > 1000.0f)
+        if (!returning && transform.position.magnitude > 1000.0f)
         {
-            Destroy(gameObject);        //destroy projectile
+            ReturnProjectile(); // Call a function to handle the return
         }
+    }
+
+    void ReturnProjectile()
+    {
+        // Reverse the direction and set the flag to indicate the projectile is returning
+        rigidbody2d.velocity = -rigidbody2d.velocity;
+        returning = true;
     }
 
     void OnCollisionEnter2D(Collision2D other)
     {
         EnemyController e = other.collider.GetComponent<EnemyController>();
 
-
         if (e != null)
         {
             e.Fix();
-
+            ReturnProjectile(); // Call the return function when hitting an enemy
         }
 
-        Destroy(gameObject);
+        // Uncomment the line below if you want the projectile to be destroyed after hitting an enemy
+        // Destroy(gameObject);
     }
 }
-
